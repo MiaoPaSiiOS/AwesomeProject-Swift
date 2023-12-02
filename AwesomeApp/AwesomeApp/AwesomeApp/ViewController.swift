c12b9dd844dc0d89ba04c2fdcddd7ea16d963bfc
zhuyuhui <zhuyuhui@odianyun.com>
2023-12-02 20:23:37 +0800

    feat: Xcode14.2 初始化项目

---
+++ AwesomeApp/AwesomeApp/AwesomeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AwesomeApp
+//
+//  Created by zhuyuhui on 2023/12/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
